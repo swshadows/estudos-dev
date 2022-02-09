7e80a2c569f536f0ed828f337a81e1095301ed03
Joelson Lima <26340441+swshadows@users.noreply.github.com>
2022-02-09 16:48:38 -0400

    Adicionando arquivos

--- 4-web-servidores (php, node, express)/node-express/vidly-api/index.js
+++ 4-web-servidores (php, node, express)/node-express/vidly-api/index.js
@@ -1,7 +1,14 @@
+const config = require("config");
 const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const mongoose = require("mongoose");
 const db = "mongodb://localhost:27017/vidly";
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined");
+  process.exit(1);
+}
 
 mongoose
   .connect(db)
@@ -10,12 +17,20 @@ mongoose
 
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rental = require("./routes/rental");
+const users = require("./routes/user");
+const auth = require("./routes/auth");
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rental", rental);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
--- 4-web-servidores (php, node, express)/node-express/vidly-api/routes/genres.js
+++ 4-web-servidores (php, node, express)/node-express/vidly-api/routes/genres.js
@@ -1,3 +1,4 @@
+const auth = require("../middleware/auth");
 const { Genre, validate } = require("../models/genres");
 const express = require("express");
 const mongoose = require("mongoose");
@@ -8,7 +9,7 @@ router.get("/", async (req, res) => {
   res.send(genres);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
