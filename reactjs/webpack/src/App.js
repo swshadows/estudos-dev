d355866182d2393bac2edc1bbc2b09a7b0c3b904
Joelson Lima <26340441+swshadows@users.noreply.github.com>
2022-03-07 16:12:58 -0400

    Adicionando alguns arquivos de React.js

---
+++ reactjs/webpack/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  //   return React.createElement("a", { href: "www.google.com.br" }, "Google.com");
+  return <a href="www.google.com.br">Google.com</a>;
+};
+
+export default App;
