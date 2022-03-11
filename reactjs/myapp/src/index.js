aaa3b1e7ef7acc245e291670eb458184cd973c32
Joelson Lima <26340441+swshadows@users.noreply.github.com>
2022-03-11 16:07:57 -0400

    Adicionando estudos Reactjs

--- reactjs/myapp/src/index.js
+++ reactjs/myapp/src/index.js
@@ -1,10 +1,15 @@
+// Para ver os feitos nos módulos, comente e descomente as tags JSX
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import ExercicioUm from "./ExercicioUm";
+import { Form } from "./Form";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* <ExercicioUm /> */}
+    <Form />
   </React.StrictMode>,
   document.getElementById("root")
 );
